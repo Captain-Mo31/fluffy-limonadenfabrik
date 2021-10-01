65ac440465becfb78a174739643f3ca8dbcef336
Moritz <65597620+Captain-Mo31@users.noreply.github.com>
2021-10-01 07:51:18 +0200

    Update _layout.svelte

--- src/pages/_layout.svelte
+++ src/pages/_layout.svelte
@@ -5,8 +5,8 @@
 </script>
 
 <Navbar />
-<div class="text-center font-serif items-center w-full prose ">
-  <main class="w-screen">
+<div class="text-center font-serif items-center w-full prose overflow-hidden">
+  <main class="w-2/3">
     <slot />
   </main>
 </div>
