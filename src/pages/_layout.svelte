25feeb14d056f1d694af5fa1370b7df8bdeca781
Caiptain-Mo <65597620+Caiptain-Mo@users.noreply.github.com>
2021-10-04 16:34:52 +0200

    Update _layout.svelte

--- src/pages/_layout.svelte
+++ src/pages/_layout.svelte
@@ -4,14 +4,15 @@
   import Footer from "../components/Footer.svelte";
 </script>
 
-<meta charset="UTF-8" />
-<meta http-equiv="X-UA-Compatible" content="IE=edge" />
-<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-<meta name="author" content="Moritz Kelle" />
-<meta name="description" content="Fluffy Limonadenfabrik website" />
-<meta name="theme-color" content="yellow-400 " />
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta name="author" content="Moritz Kelle" />
+  <meta name="description" content="Fluffy Limonadenfabrik website" />
+  <meta name="theme-color" content="yellow-400 " />
+</head>
 <body class="bg-gray-50 w-full h-auto dark:bg-gray-700  ">
-  
   <Navbar />
   <div
     class=" overflow-hidden font-serif w-full grid justify-items-center dark:text-white"
