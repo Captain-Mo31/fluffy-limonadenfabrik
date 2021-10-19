dc72c9fd6c78418444c5815bfa03f707bee7b09a
Caiptain-Mo <65597620+Caiptain-Mo@users.noreply.github.com>
2021-10-19 17:15:11 +0200

    Update _fallback.svelte

--- src/pages/_fallback.svelte
+++ src/pages/_fallback.svelte
@@ -3,35 +3,9 @@
   import ErrorImage from "../components/ErrorImage.svelte";
 </script>
 
-<main class="bg-white  overflow-hidden h-screen relative">
-  <header class="absolute top-0 left-0 right-0 z-20">
-    <nav class="container mx-auto px-6 md:px-12 py-4">
-      <div class="md:flex justify-between items-center">
-        <div class="flex justify-between items-center">
-          <div class="md:hidden">
-            <button class="text-gray-800 focus:outline-none">
-              <svg
-                class="h-12 w-12"
-                viewBox="0 0 24 24"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M4 6H20M4 12H20M4 18H20"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                />
-              </svg>
-            </button>
-          </div>
-        </div>
-      </div>
-    </nav>
-  </header>
+<div class="bg-white  overflow-hidden h-screen relative">
   <div
-    class="container mx-auto h-screen pt-32 md:pt-0 px-6 z-10 flex items-center justify-between"
+    class="container mx-auto pt-32 md:pt-0 px-6 z-10 flex items-center justify-between"
   >
     <div
       class="container mx-auto px-6 flex flex-col-reverse lg:flex-row justify-between items-center relative"
@@ -55,4 +29,4 @@
       </div>
     </div>
   </div>
-</main>
+</div>
