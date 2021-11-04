54d34791ba0f35526e8f3bdf66a7a6526392e701
Caiptain-Mo <65597620+Caiptain-Mo@users.noreply.github.com>
2021-11-04 13:42:11 +0100

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -14,19 +14,23 @@
       font-family: "apfel";
       src: url("/fonts/apple/ApfelGrotezk-Fett.woff2") format("woff2"),
         url("/fonts/apple/ApfelGrotezk-Fett.woff") format("woff");
+      font-display: swap;
     }
     @font-face {
       font-family: "apfel";
       src: url("/fonts/apple/ApfelGrotezk-Regular.woff2") format("woff2"),
         url("/fonts/apple/ApfelGrotezk-Regular.woff") format("woff");
+      font-display: swap;
     }
     @font-face {
       font-family: "montserat";
       src: url("public\fonts\montserrat\Montserrat-Regular.ttf") format("ttf");
+      font-display: swap;
     }
     @font-face {
       font-family: "montserat";
       src: url("public\fonts\montserrat\Montserrat-Bold.ttf") format("ttf");
+      font-display: swap;
     }
   }
 </style>
